6fc5840a919cc091d0aded5d62515b8fd64b2e39
hemanth2793 <hemanth.s@pacewisdom.com>
2017-05-30 10:41:31 +0530

    hl

--- src/test/java/Test/TestDemo/NewTest.java
+++ src/test/java/Test/TestDemo/NewTest.java
@@ -1,5 +1,7 @@
 package Test.TestDemo;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
@@ -11,6 +13,7 @@ public class NewTest {
   public void test() {
 	  System.setProperty("webdriver.firefox.marionette","/home/pace/Downloads/geckodriver.exe");
 	  driver= new FirefoxDriver();
+	  driver.manage().timeouts().implicitlyWait(45, TimeUnit.SECONDS);
 	  driver.get("http://qaclickacademy.com");
 		System.out.println(driver.getTitle());
 		System.out.println(driver.getCurrentUrl());
